2d8f8b941d680dcc39e9dac0b6f1214a33f0a398
Eric Adamski <er.adamski@gmail.com>
2018-07-31 08:31:58 -0400

    Update the slide to have the data directly in the slide

--- 5.js
+++ 5.js
@@ -1,7 +1,19 @@
-const db = require("./src/db.js");
+const user = {
+  repo_count: 72,
+  star_count: 225,
+  _id: "5b391c5bb1fe6d49210b9884",
+  name: "45726963204164616d736b69",
+  login: "657269636164616d736b69",
+  avatar_url:
+    "68747470733a2f2f61766174617273302e67697468756275736572636f6e74656e742e636f6d2f752f363531363735383f763d34",
+  location_id: "5b390c9cc941da1b67fa72a1",
+  url:
+    "68747470733a2f2f6170692e6769746875622e636f6d2f75736572732f657269636164616d736b69"
+};
 
-// Image we have some data like so:
-console.clear();
-db.User.findOne()
-  .then(user => console.log(JSON.stringify(user.toJSON(), null, 2)))
-  .then(() => db.close());
+// const db = require("./src/db.js");
+
+// console.clear();
+// db.User.findOne()
+//   .then(user => console.log(JSON.stringify(user.toJSON(), null, 2)))
+//   .then(() => db.close());
